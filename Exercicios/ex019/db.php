f8a03a30f2e608e09c6916b07e898c3aeaed8b19
João Pedro <joao.fernandes37.pedro@gmail.com>
2024-12-15 11:21:50 -0300

    Atualizei

--- Exercicios/ex019/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Conexão com o banco de dados SQLite
-function conectarBanco() {
-    $db = new PDO('sqlite:../database.sqlite');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $db;
-}
-?>
